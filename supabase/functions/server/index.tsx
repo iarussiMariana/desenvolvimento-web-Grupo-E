20e9536b7dea6ec414b54fc15d6f9f02b17445b7
OFamosoCientista <vinicius.maxboy22@gmail.com>
2026-05-07 20:14:37 -0300

    1.27.301

--- supabase/functions/server/index.tsx
+++ supabase/functions/server/index.tsx
@@ -1,5 +1,5 @@
-import { Hono } from "npm:hono";
-import { cors } from "npm:hono/cors";
+import { Hono } from "hono";
+import { cors } from "cors";
 import { logger } from "npm:hono/logger";
 import { createClient } from "npm:@supabase/supabase-js";
 import * as kv from "./kv_store.tsx";
